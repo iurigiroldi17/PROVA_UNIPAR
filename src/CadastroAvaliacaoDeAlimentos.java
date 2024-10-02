5d0d1ef567cd52ef3fed55f43c4c289ae8a7e18b
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-10-02 20:33:58 -0300

    primeiro commit

--- src/CadastroAvaliacaoDeAlimentos.java
+++ src/CadastroAvaliacaoDeAlimentos.java
@@ -8,14 +8,14 @@ public class CadastroAvaliacaoDeAlimentos {
         System.out.print("Digite a quantidade de alimentos: ");
         int numAlimentos = scanner.nextInt();
 
-        // Criar um vetor para armazenar as avaliações
+        // Cria um vetor para armazenar as avaliações
         double[] avaliacoes = new double[numAlimentos];
 
         // Loop para coletar as avaliações
         for (int i = 0; i < numAlimentos; i++) {
             double nota;
             while (true) {
-                System.out.printf("Digite a avaliação para o alimento  (entre 0 e 10): ", i + 1);
+                System.out.printf("Digite a avaliação para o alimento %d (entre 0 e 10): ", i + 1);
                 nota = scanner.nextDouble();
 
                 if (nota >= 0 && nota <= 10) {
@@ -35,17 +35,8 @@ public class CadastroAvaliacaoDeAlimentos {
         double media = soma / numAlimentos;
 
         // Exibe a média
-        System.out.printf("A média das avaliações é: ", media);
+        System.out.printf("A média das avaliações é: %.2f%n", media);
 
         scanner.close();
     }
 }
-
-
-
-
-
-
-
-
-
---
+++ src/VotacaoJogoDeFutebol.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VotacaoJogoDeFutebol {
+    public static void main(String[] args) {
+
+        Scanner SC = new Scanner(System.in);
+        int[] votos = new int [10];
+        System.out.print("Digite a quantidade de votantes: ");
+        int quantidade = SC.nextInt();
+
+        int votoPessoa1 = 10;
+        int votoPessoa2 = 20;
+        int votoPessoa3 = 50;
+        int votoPessoa4 = 70;
+        int votoPessoa5 = 60;
+        int votoPessoa6 = 60;
+        int votoPessoa7 = 70;
+        int votoPessoa8 = 70;
+        int votoPessoa9 = 90;
+        int votoPessoa10 = 90;
+
+        votos[0] = 50;
+        votos[1] = 60;
+        votos[2] = 70;
+        votos[3] = 80;
+    }
+}
